6443f74da67793e23b4046869d2a03fcc5c4cd5c
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-01-24 14:30:43 +0100

    Be less greedy with extensions (ref #2)

--- src/index.js
+++ src/index.js
@@ -4,20 +4,6 @@ const mix = require('laravel-mix');
 const omit = require('lodash/omit');
 const PurgecssPlugin = require('purgecss-webpack-plugin');
 
-const defaults = {
-    enabled: mix.inProduction(),
-    globs: [
-        path.resolve(__dirname, '../../../app/**/*.php'),
-        path.resolve(__dirname, '../../../resources/**/*.js'),
-        path.resolve(__dirname, '../../../resources/**/*.jsx'),
-        path.resolve(__dirname, '../../../resources/**/*.ts'),
-        path.resolve(__dirname, '../../../resources/**/*.tsx'),
-        path.resolve(__dirname, '../../../resources/**/*.php'),
-        path.resolve(__dirname, '../../../resources/**/*.vue'),
-    ],
-    extensions: ['html', 'js', 'jsx', 'ts', 'tsx', 'php', 'vue'],
-};
-
 function createPlugin(options) {
     return new PurgecssPlugin({
         paths: () => glob.sync(options.globs),
@@ -35,25 +21,41 @@ function createPlugin(options) {
     });
 }
 
+function createOptions(options) {
+    options.enabled = options.enabled || mix.inProduction();
+
+    options.extensions = options.extensions || [
+        'html',
+        'js',
+        'jsx',
+        'ts',
+        'tsx',
+        'php',
+        'vue',
+    ];
+
+    options.globs = options.globs || [];
+
+    options.globs.push(
+        path.resolve(__dirname, '../../../app/**/*.php'),
+        ...options.extensions.map(extension =>
+            path.resolve(__dirname, `../../../resources/**/*.${extension}`)
+        )
+    );
+
+    return options;
+}
+
 function withoutCustomOptions(options) {
     return omit(options, ['enabled', 'globs', 'extensions']);
 }
 
 mix.purgeCss = (options = {}) => {
-    // We don't want to overwrite the globs so the user can append instead of
-    // replace. If it needs to be replaced instead, use the underlying `paths`
-    // option.
-    const globs = defaults.globs.concat(options.globs || []);
-
-    options = { ...defaults, ...options, globs };
+    options = createOptions(options);
 
     if (options.enabled) {
         mix.webpackConfig({
             plugins: [createPlugin(options)],
         });
     }
-
-    return mix;
 };
-
-module.exports.defaults = defaults;
