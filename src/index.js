869ff18597deaa4eb75176d6c8ff4384346819f8
CJMAXiK <dj.cjmaxik@gmail.com>
2018-03-18 11:01:50 +1000

    2.0.0

--- src/index.js
+++ src/index.js
@@ -1,67 +1,74 @@
-const path = require('path');
-const glob = require('glob-all');
-const mix = require('laravel-mix');
-const omit = require('lodash/omit');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
+let mix = require('laravel-mix');
 
-function createPlugin(options) {
-    return new PurgecssPlugin(
-        Object.assign(
-            {
-                paths: () => glob.sync(options.globs),
-                extractors: [
-                    {
+class purgeCss {
+    name() {
+        return 'purgeCss';
+    }
+
+    dependencies() {
+        this.requiresReload = true;
+
+        return ['purgecss-webpack-plugin', 'glob-all', 'lodash.omit'];
+    }
+
+    static userOptions(userOptions) {
+        let options = Object.assign({
+                enabled: Mix.inProduction(),
+                extensions: [
+                    'html',
+                    'js',
+                    'jsx',
+                    'ts',
+                    'tsx',
+                    'php',
+                    'vue',
+                ],
+                globs: []
+            },
+            userOptions
+        );
+
+        options.globs.push(
+            path.resolve(__dirname, 'app/**/*.php'),
+            ...options.extensions.map(extension =>
+                path.resolve(__dirname, `resources/**/*.${extension}`)
+            )
+        );
+
+        return options;
+    }
+
+    register(userOptions = {}) {
+        this.options = purgeCss.userOptions(userOptions);
+    }
+
+    static withoutCustomOptions(options) {
+        let omit = require('lodash.omit');
+        return omit(options, ['enabled', 'globs', 'extensions']);
+    }
+
+    webpackConfig(config) {
+        let PurgecssPlugin = require('purgecss-webpack-plugin');
+        let path = require('path');
+        let glob = require('glob-all');
+
+        if (this.options.enabled) {
+            config.plugins.push(new PurgecssPlugin(Object.assign({
+                    paths: () => glob.sync(this.options.globs),
+                    extractors: [{
                         extractor: class {
                             static extract(content) {
                                 return content.match(/[A-z0-9-:\/]+/g) || [];
                             }
                         },
-                        extensions: options.extensions,
-                    },
-                ],
-            },
-            withoutCustomOptions(options)
-        )
-    );
-}
-
-function createOptions(options) {
-    options.enabled = options.enabled || mix.inProduction();
-
-    options.extensions = options.extensions || [
-        'html',
-        'js',
-        'jsx',
-        'ts',
-        'tsx',
-        'php',
-        'vue',
-    ];
-
-    options.globs = options.globs || [];
-
-    options.globs.push(
-        path.resolve(__dirname, '../../../app/**/*.php'),
-        ...options.extensions.map(extension =>
-            path.resolve(__dirname, `../../../resources/**/*.${extension}`)
-        )
-    );
-
-    return options;
-}
-
-function withoutCustomOptions(options) {
-    return omit(options, ['enabled', 'globs', 'extensions']);
-}
-
-mix.purgeCss = (options = {}) => {
-    options = createOptions(options);
-
-    if (options.enabled) {
-        mix.webpackConfig({
-            plugins: [createPlugin(options)],
-        });
+                        extensions: this.options.extensions
+                    }]
+                },
+                purgeCss.withoutCustomOptions(this.options)
+            )));
+        }
     }
+}
 
-    return mix;
-};
+mix.extend('purgeCss', new purgeCss());
+
