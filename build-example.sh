a085404d69bee90d2f242d12efa3cf2cd03ebffa
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-04-22 11:21:29 +0200

    Update example build script

--- build-example.sh
+++ build-example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-rm -rf node_modules ./example/public
-cd ./example && yarn upgrade && yarn production
+rm -rf node_modules example/node_modules example/public
+cd example && yarn && yarn production
